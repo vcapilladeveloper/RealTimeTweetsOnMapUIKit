b84f127a54c24c86a91283c4b4ce34784494ab00
Victor Capilla <victorcapilladeveloper@gmail.com>
2021-08-29 10:29:59 +0200

    Initial Commit

---
+++ RealTimeTweetsOnMapUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealTimeTweetsOnMapUIKit
+//
+//  Created by Victor Capilla Developer on 29/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
